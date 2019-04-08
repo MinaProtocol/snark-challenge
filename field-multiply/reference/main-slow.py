010a9acca9d2b004cba055d5d6fb1c6bab6cff00
Joel Krauska <jkrauska@gmail.com>
2019-04-08 15:11:35 -0700

    min

--- field-multiply/reference/main-slow.py
+++ field-multiply/reference/main-slow.py
@@ -23,7 +23,7 @@ class Field:
         return Field((self.value + other.value) % field_order)
 
     def __mul__(self, other):
-        time.sleep(0.0000001)
+        time.sleep(0.000001)
         return Field((self.value * other.value) % field_order)
 
     def __eq__(self, other):
