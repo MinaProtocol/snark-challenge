9e578d4c68660bce9511f4053a6cc71ce56809d1
Izaak Meckler <ihmeckler@gmail.com>
2019-05-13 14:35:45 -0700

    reference cubic extension

---
+++ reference-cubic-extension/libff/generate_inputs.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+#include <libff/algebra/curves/mnt753/mnt6753/mnt6753_pp.hpp>
+#include <libff/common/rng.hpp>
+
+using namespace libff;
+
+void write_mnt6_fq(FILE* output, Fq<mnt6753_pp> x) {
+  fwrite((void *) x.mont_repr.data, libff::mnt6753_q_limbs * sizeof(mp_size_t), 1, output);
+}
+
+void write_mnt6_fq3(FILE* output, Fqe<mnt6753_pp> x) {
+  write_mnt6_fq(output, x.c0);
+  write_mnt6_fq(output, x.c1);
+  write_mnt6_fq(output, x.c2);
+}
+
+int main(void)
+{
+    mnt6753_pp::init_public_params();
+
+    auto output = fopen("inputs", "w");
+
+    size_t num_instances = 10;
+
+    for (size_t j = 0; j < num_instances; ++j) {
+      size_t n = 1 << (10 + j);
+
+      fwrite((void *) &n, sizeof(size_t), 1, output);
+
+      uint64_t offset = rand();
+      for (size_t i = 0; i < n; ++i) {
+        Fq<mnt6753_pp> c0 = SHA512_rng<Fq<mnt6753_pp>>(offset + 3 * i);
+        Fq<mnt6753_pp> c1 = SHA512_rng<Fq<mnt6753_pp>>(offset + 3 * i + 1);
+        Fq<mnt6753_pp> c2 = SHA512_rng<Fq<mnt6753_pp>>(offset + 3 * i + 2);
+        write_mnt6_fq3(output, Fqe<mnt6753_pp>(c0, c1, c2));
+      }
+    }
+}
+
---
+++ reference-cubic-extension/libff/main.cpp
@@ -0,0 +1,61 @@
+#include <cstdio>
+#include <vector>
+
+#include <libff/algebra/curves/mnt753/mnt6753/mnt6753_pp.hpp>
+
+using namespace libff;
+
+void write_mnt6_fq(FILE* output, Fq<mnt6753_pp> x) {
+  fwrite((void *) x.mont_repr.data, libff::mnt6753_q_limbs * sizeof(mp_size_t), 1, output);
+}
+
+Fq<mnt6753_pp> read_mnt6_fq(FILE* input) {
+  // bigint<mnt6753_q_limbs> n;
+  Fq<mnt6753_pp> x;
+  fread((void *) x.mont_repr.data, libff::mnt6753_q_limbs * sizeof(mp_size_t), 1, input);
+  return x;
+}
+
+void write_mnt6_fq3(FILE* output, Fqe<mnt6753_pp> x) {
+  write_mnt6_fq(output, x.c0);
+  write_mnt6_fq(output, x.c1);
+  write_mnt6_fq(output, x.c2);
+}
+
+Fqe<mnt6753_pp> read_mnt6_fq3(FILE* input) {
+  Fq<mnt6753_pp> c0 = read_mnt6_fq(input);
+  Fq<mnt6753_pp> c1 = read_mnt6_fq(input);
+  Fq<mnt6753_pp> c2 = read_mnt6_fq(input);
+  return Fqe<mnt6753_pp>(c0, c1, c2);
+}
+
+// The actual code for doing Fq3 multiplication lives in libff/algebra/fields/fp3.tcc
+int main(void)
+{
+    mnt6753_pp::init_public_params();
+
+    auto inputs = fopen("inputs", "r");
+    auto outputs = fopen("outputs", "w");
+
+    while (true) {
+      size_t n;
+      size_t elts_read = fread((void *) &n, sizeof(size_t), 1, inputs);
+
+      if (elts_read == 0) { break; }
+
+      std::vector<Fqe<mnt6753_pp>> x;
+      for (size_t i = 0; i < n; ++i) {
+        x.emplace_back(read_mnt6_fq3(inputs));
+      }
+
+      Fqe<mnt6753_pp> out_x = Fqe<mnt6753_pp>::one();
+      for (size_t i = 0; i < n; ++i) {
+        out_x = out_x * x[i];
+      }
+
+      write_mnt6_fq3(outputs, out_x);
+    }
+    fclose(outputs);
+
+    return 0;
+}
