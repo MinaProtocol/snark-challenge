7896c0696efe60eb8f2bf0c12dc30bac3ab1e1cc
Joel Krauska <jkrauska@gmail.com>
2019-06-19 15:13:41 -0700

    strech out steps

--- reference-05-verifier/build.sh
+++ reference-05-verifier/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-dune build crypto_lib/crypto_lib.bc.js && tsc verifier.ts
+
+set -x
+
+# Use js_of_ocaml to generate crypto_lib javascript
+dune build crypto_lib/crypto_lib.bc.js
+
+# Use typescript compiler to generate javascript
+tsc verifier.ts
+
+# put tools in dist dir
 cp _build/default/crypto_lib/crypto_lib.bc.js dist/crypto_lib.js
 mv verifier.js dist/
